5a74d27dd4fa3fcaab87832a5a5b3beb059c0e65
AidanMcClean <admcclea@asu.edu>
2024-05-07 13:33:28 -0700

    Add project files.

---
+++ NumberGuessAppNET/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace NumberGuessAppNET.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly HttpClient _client = new HttpClient();
+
+        public ActionResult Index()
+        {
+            ViewBag.AttemptCount = Session["AttemptCount"] ?? 0;
+            ViewBag.Result = Session["Result"] ?? string.Empty;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> SecretNumber(int lowerLimit, int upperLimit)
+        {
+            //response to service
+            var response = await _client.GetAsync($"http://localhost:50483/Service1.svc/SecretNumber/{lowerLimit}/{upperLimit}");
+            if (response.IsSuccessStatusCode)
+            {
+                var secretNumberXml = await response.Content.ReadAsStringAsync();
+                var secretNumber = XElement.Parse(secretNumberXml).Value;
+                //stores secret number in session and intializes attempts to 0
+                Session["SecretNumber"] = secretNumber;
+                Session["AttemptCount"] = 0;
+                return Json(new { secretNumber });
+            }
+            return Json(new { error = "secret number not generated" });
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> CheckNumber(int userGuess)
+        {
+            //fetch secretnumber from the session
+            var secretNumber = Session["SecretNumber"]?.ToString();
+            //response to service
+            var response = await _client.GetAsync($"http://localhost:50483/Service1.svc/CheckNumber/{userGuess}/{secretNumber}");
+            if (response.IsSuccessStatusCode)
+            {
+                var resultXml = await response.Content.ReadAsStringAsync();
+                var result = XElement.Parse(resultXml).Value;
+                //increments attempt count in session
+                Session["AttemptCount"] = (int)(Session["AttemptCount"] ?? 0) + 1;
+                return Json(new { result = result, attemptCount = Session["AttemptCount"] });
+            }
+            return Json(new { result = "couldnt check guess", attemptCount = Session["AttemptCount"] });
+        }
+    }
+}
---
+++ NumberGuessRest/Service1.svc.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.SessionState;
+
+namespace NumberGuessRest
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        private static int sNumber;
+
+        public int SecretNumber(string lower, string upper)
+        {
+            int lowerNum = Convert.ToInt32(lower);
+            int upperNum = Convert.ToInt32(upper);
+
+            //generates a random number that will be stored as a secret number
+            DateTime currentDate = DateTime.Now;
+            int seed = (int)currentDate.Ticks;
+            Random random = new Random(seed);
+            int sNumber = random.Next(lowerNum, upperNum);
+            return sNumber;
+        }
+
+        public string CheckNumber(string userNum, string SecretNum)
+        {
+            //checks if the user number is equal to the secret number, if so the function returns correct
+
+            int userNumber = Convert.ToInt32(userNum);
+            int secretNumber = Convert.ToInt32(SecretNum);
+
+            if (userNumber == secretNumber)
+            {
+                return "correct";
+            }
+            else
+            {
+                if (userNumber > secretNumber)
+                {
+                    return "too big";
+                }
+                else
+                {
+                    return "too small";
+                }
+            }
+        }
+    }
+}
